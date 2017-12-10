22d83253c46a2c53deeb0f9c6f0bcbfcbf961afe
Gotesu <gotesuriel@gmail.com>
2017-12-10 20:31:35 +0200

    server reading updated

--- Server/main.cpp
+++ Server/main.cpp
@@ -6,8 +6,27 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+int stringToInt(string input) {
+    int number = 0;
+    int dig;
+    for (int i = 0; i < input.size(); i ++) {
+        dig = (int) (input.at(i) - '0');
+        number += (pow(10, input.size() - 1 - i) * dig);
+    }
+    return number;
+}
+
 int main() {
-    Server server(9999);
+    fstream data;
+    string input;
+    try {
+        data.open("Server_Settings.txt");
+    } catch(exception) {
+        throw "Cannot open settings file";
+    }
+    getline(data, input);
+    Server server(stringToInt(input));
     try {
         server.start();
     } catch (const char *msg) {
