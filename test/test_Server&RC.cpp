1b25daffd790b49b0321fbf93a90c4dd44deed3f
Gotesu <gotesuriel@gmail.com>
2017-12-12 19:07:37 +0200

    test update

--- test/test_Server&RC.cpp
+++ test/test_Server&RC.cpp
@@ -13,27 +13,28 @@ enum Side {WHITE = -1, EMPTY = 0, BLACK = 1};
  * the test checks the getSide function.
  */
 TEST(Connection, checkSides) {
-	Server server(50000);
-	try {
-		server.start();
-		RemoteConnection rc = RemoteConnection();
-		rc.connectToServer();
-		RemoteConnection rc2 = RemoteConnection();
-		rc2.connectToServer();
-    int x = rc.getSide();
-    int o = rc2.getSide();
-    // check sides
-    EXPECT_EQ(x, BLACK);
-    EXPECT_EQ(o, WHITE);
-	  // Disconnect
-		char send[10] = {0};
-		send[0] = 'E';
-		send[1] = 'n';
-		send[2] = 'd';
-		rc.sendMove(send);
-		rc2.sendMove(send);
-	} catch (exception& e) {
-		cout << "Error";
+    Server server(50000);
+    try {
+        server.start();
+        RemoteConnection rc = RemoteConnection();
+        rc.connectToServer();
+        RemoteConnection rc2 = RemoteConnection();
+        rc2.connectToServer();
+        int x = rc.getSide();
+        int o = rc2.getSide();
+        // check sides
+        EXPECT_EQ(x, BLACK);
+        EXPECT_EQ(o, WHITE);
+        // Disconnect
+        char send[10] = {0};
+        send[0] = 'E';
+        send[1] = 'n';
+        send[2] = 'd';
+        rc.sendMove(send);
+        rc2.sendMove(send);
+    } catch (exception &e) {
+        cout << "Error";
+    }
 }
 
 /**********************
@@ -41,30 +42,31 @@ TEST(Connection, checkSides) {
  * the test checks sendMove and getMove function.
  */
 TEST(Connection, sendAndGet) {
-	Server server(50000);
-	try {
-		server.start();
-		// create clients and connect them
-		RemoteConnection rc = RemoteConnection();
-		rc.connectToServer();
-		RemoteConnection rc2 = RemoteConnection();
-		rc2.connectToServer();
-		rc.getSide();
-		rc2.getSide();
-		// send and get a move
-		char send[10] = {0}, get[10] = {0};
-		send[0] = 'O';
-		send[1] = 'K';
-		rc.sendMove(char* send)
-		rc2.getMove(get);
-		// check the result
-	  EXPECT_EQ(strcmp(get, "OK"), 0);
-	  // Disconnect
-		send[0] = 'E';
-		send[1] = 'n';
-		send[2] = 'd';
-		rc.sendMove(send);
-		rc2.sendMove(send);
-	} catch (exception& e) {
-		cout << "Error";
+    Server server(50000);
+    try {
+        server.start();
+        // create clients and connect them
+        RemoteConnection rc = RemoteConnection();
+        rc.connectToServer();
+        RemoteConnection rc2 = RemoteConnection();
+        rc2.connectToServer();
+        rc.getSide();
+        rc2.getSide();
+        // send and get a move
+        char send[10] = {0}, get[10] = {0};
+        send[0] = 'O';
+        send[1] = 'K';
+        rc.sendMove(send);
+        rc2.getMove(get);
+        // check the result
+        EXPECT_EQ(strcmp(get, "OK"), 0);
+        // Disconnect
+        send[0] = 'E';
+        send[1] = 'n';
+        send[2] = 'd';
+        rc.sendMove(send);
+        rc2.sendMove(send);
+    } catch (exception &e) {
+        cout << "Error";
+    }
 }
